714a8db45247e514f1bd3df7eb8c33241dc8a077
lithial <jameshessell@gmail.com>
2021-09-08 12:00:40 +1200

    Feat: moved data to the main app component for now and passed it as a prop to the dial

--- src/App.js
+++ src/App.js
@@ -3,9 +3,50 @@ import Dial from "./components/dial/dial";
 import Test from "./components/dial/test";
 
 
+const indicators = [
+  {
+    type: "lit",
+  },
+  {
+    type: "unlit",
+  },
+  {
+    type: "unlit",
+  },
+  {
+    type: "unlit",
+  },
+  {
+    type: "lit",
+  },
+  {
+    type: "red",
+  },
+  {
+    type: "red",
+  },
+  {
+    type: "lit",
+  },
+  {
+    type: "unlit",
+  },
+  {
+    type: "lit",
+  },
+  {
+    type: "red",
+  },
+  {
+    type: "red",
+  },
+  {
+    type: "lit",
+  },
+]
 function App() {
   return (<div className="App">
-    <Test/>
+    <Dial circleName="Circle Name" indicators={indicators}/>
   </div>);
 }
 
