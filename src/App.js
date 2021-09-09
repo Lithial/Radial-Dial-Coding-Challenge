7cef5cbdb63119866db93bbe2badb46687d4ea37
lithial <jameshessell@gmail.com>
2021-09-09 18:42:20 +1200

    feat: app flex rework. Changed to be more consistent

--- src/App.js
+++ src/App.js
@@ -1,93 +1,13 @@
 import './App.scss';
 import Dial from "./components/dial/dial";
+import StateChanger from "./components/stateChanger/stateChanger";
 
-
-const indicators = [
-  {
-    type: "red",
-  },
-  {
-    type: "red",
-  },
-  {
-    type: "lit",
-  },
-  {
-    type: "lit",
-  },
-  {
-    type: "unlit",
-  },
-  {
-    type: "unlit",
-  },
-  {
-    type: "red",
-  },
-  {
-    type: "red",
-  },
-  {
-    type: "lit",
-  },
-  {
-    type: "unlit",
-  },
-  {
-    type: "lit",
-  },
-  {
-    type: "red",
-  },
-  // {
-  //   type: "red",
-  // },
-  // {
-  //   type: "lit",
-  // },
-  // {
-  //   type: "lit",
-  // },
-  // {
-  //   type: "unlit",
-  // },
-  // {
-  //   type: "unlit",
-  // },
-  // {
-  //   type: "unlit",
-  // },
-  // {
-  //   type: "lit",
-  // },
-  // {
-  //   type: "red",
-  // },
-  // {
-  //   type: "red",
-  // },
-  // {
-  //   type: "lit",
-  // },
-  // {
-  //   type: "unlit",
-  // },
-  // {
-  //   type: "lit",
-  // },
-  // {
-  //   type: "red",
-  // },
-  // {
-  //   type: "red",
-  // },
-  // {
-  //   type: "lit",
-  // },
-]
 function App() {
   return (<div className="app">
-    <Dial circleName="Circle Name" indicators={indicators}/>
+    <div className="flex">
+      <Dial circleName="Circle Name"/>
+      <StateChanger/>
+    </div>
   </div>);
 }
 
